b44d37df90eba11095e7a544da04a2180e6df1a4
Frison B Alexander <fri.b.alex@gmail.com>
2017-07-29 08:03:19 -0400

    ListBinding

--- FirstSteps/MainWIndowViewModel.cs
+++ FirstSteps/MainWIndowViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel; //ObservableCollection
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 //Install-Package Prism.Core
 using Prism.Mvvm; //BindableBase
 
 //Install-Package Microsoft.Tpl.Dataflow
 using System.Threading.Tasks.Dataflow; // Blocks :)
+
 
 namespace FirstSteps
 {
@@ -24,12 +27,26 @@ namespace FirstSteps
             }
         }
 
+        //TODO: DependencyProperties
+
+        //TODO: Command: both routed and delegate
+
+        //TODO: AttachedProperties
         
+        //TODO: ValueConverters (will AttachedProperties work better in converting a ViewModel value to a view element property?)
+
+
+    
+        public class TwoNumbers { public int X { get; set; } public int Y { get; set; } }
+        public ObservableCollection<TwoNumbers> RandomTwoNumbers { get; set; }
 
         public MainWIndowViewModel()
         {
+            RandomTwoNumbers = new ObservableCollection<TwoNumbers>();
+
             this.ExampleOfTextBinding = $"This is from the view model constructor from threadId={System.Threading.Thread.CurrentThread.ManagedThreadId}";
             Task.Run(ChangeText);
+            Task.Run(GenerateRandomTwoNumbers);
         }
 
         private async Task ChangeText()
@@ -37,7 +54,26 @@ namespace FirstSteps
             await Task.Delay(3000);
             
             this.ExampleOfTextBinding = $"And this is after its been changed from an async task from threadId={System.Threading.Thread.CurrentThread.ManagedThreadId}";
+        }
 
+        private async Task GenerateRandomTwoNumbers()
+        {
+            //return now and let the schedular worry about where its going to run...
+            await Task.Yield();
+
+            var r = new Random(DateTime.Now.Millisecond);
+
+            while (true)
+            {
+                //Got to create the TwoNumbers object (because of thread affinity - STA) and add it to the collection on the UI thread..
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var xy = new TwoNumbers { X = r.Next(100), Y = r.Next(100) };
+                    RandomTwoNumbers.Add(xy);
+                });
+
+                await Task.Delay(100);
+            }
         }
 
     }
